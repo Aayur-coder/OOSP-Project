0fc2702283c8c1b677d4b65eaf5c91f6620b61f5
Aayur-coder <aayurguleria@gmail.com>
2025-11-15 23:45:16 +0530

    The patient class

--- include/Doctor.h
+++ include/Doctor.h
@@ -1,3 +1,28 @@
-#ifndef Doctor_H
-#define Doctor_H
-#endif   
+#ifndef DOCTOR_H
+#define DOCTOR_H
+
+#include "Person.h"
+#include <string>
+using std::string;
+
+// Doctor class inherits from Person
+class Doctor : public Person {
+private:
+    int doctorID;
+    string specialization;
+    string availability;
+
+public:
+    // Function declarations
+    void inputInfo() override;       // Polymorphism
+    void displayInfo() override;     // Polymorphism
+
+    // Getter function
+    int getDoctorID() const;
+
+    // For file handling (later in .cpp)
+    string toFileString() const;
+    void fromFileString(const string& line);
+};
+
+#endif
