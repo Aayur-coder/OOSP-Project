b102163f9b31848a3712444af6166104728238d2
Arun Thakur <arunthakur413f@gmail.com>
2025-11-15 23:49:34 +0530

    Update Patient.h

--- include/Patient.h
+++ include/Patient.h
@@ -1,4 +1,31 @@
 #ifndef PATIENT_H
-#define PATIENT_H   
+#define PATIENT_H
+
+#include "Person.h"
+#include <string>
+using std::string;
+
+class Patient : public Person {
+private:
+    int patientID;
+    string disease;
+    int assignedDoctorID;   
+public:
+
+    void inputInfo() override;       
+    void displayInfo() override;    
+
+    int getPatientID() const;
+    int getAssignedDoctorID() const;
+
+
+    void setAssignedDoctorID(int id);
+
+    string toFileString() const;
+    void fromFileString(const string& line);
+};
+
+#endif
+
 
 
